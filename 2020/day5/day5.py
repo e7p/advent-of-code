ccd458eb5e87d39c7a50f05d41459a6be68e68d5
e7p <git@e7p.de>
2020-12-06 00:18:07 +0100

    solution of day 5

---
+++ 2020/day5/day5.py
@@ -0,0 +1,13 @@
+# parse data
+data = [line.strip() for line in open("input")]
+
+# to keep straightforward, the seat ID generates directly from the binary conversion of the "FBLR" encoded string
+seat_ids = [int(x.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2) for x in data]
+
+# just take the maximum value there
+result1 = max(seat_ids)
+print(f"part 1: {result1}")
+
+# iterate over each possible seat ID which can have a predecessor or sucessor and only select those that are unoccupied
+result2 = [i for i in range(min(seat_ids) + 1, result1) if i not in seat_ids]
+print(f"part 2: {result2}")
