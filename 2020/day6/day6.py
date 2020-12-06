745b8cd1a92818b6ee00c5784b71edf4844f715c
e7p <git@e7p.de>
2020-12-06 06:51:50 +0100

    getting rid of newline characters

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -2,9 +2,9 @@
 data = [line.strip() for line in open("input").read().split("\n\n")]
 
 # sum up all unique characters in each block, excluding the newlines separating each answer
-result1 = sum(len(set(x.replace("\n", ""))) for x in data)
+result1 = sum(len(set("".join(x.split()))) for x in data)
 print(f"part 1: {result1}")
 
 # sum up all characters that are in all answers of every block by iterating through the first answer's characters 
-result2 = sum(sum(all(y in x for x in z.split("\n")) for y in z.split("\n")[0]) for z in data)
+result2 = sum(sum(all(y in x for x in z.split()) for y in z.split()[0]) for z in data)
 print(f"part 2: {result2}")
